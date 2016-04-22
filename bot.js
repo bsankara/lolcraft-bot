1d7a335fab1be8ec2910809e1e5bef0be28ab3d8
Balaji Sankar <balajisankar1@gmail.com>
2016-04-22 11:08:57 -0600

    Initial Commit with basic files.

---
+++ bot.js
@@ -0,0 +1,16 @@
+var Discord = new require("discord.js");
+var fs = require("fs");
+
+var bot = new Discord.Client();
+
+bot.on("message", function(message) {
+    if (message.content === "Hi") {
+        bot.reply(message, "Hey there!");
+    } else if (message.content === "Kill") {
+        bot.logout();
+        process.exit(0);
+    }
+});
+var obj = JSON.parse(fs.readFileSync('settings.json', 'utf8'));
+bot.login(obj.loginUserName, obj.loginPassword).then();
+
